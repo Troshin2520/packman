035ab048cce25b81078af968d9e56b6b4fbe59b7
Troshin2520 <troshin321@gmail.com>
2018-03-07 21:52:32 +0000

    fixed less-loader using

---
+++ scripts/customized-config.js
@@ -0,0 +1,31 @@
+var rewire = require('rewire');
+var proxyquire = require('proxyquire');
+
+switch(process.argv[2]) {
+    case 'start':
+        rewireModule('react-scripts/scripts/start.js', loadCustomizer('../webpack.config'));
+        break;
+    case 'build':
+        rewireModule('react-scripts/scripts/build.js', loadCustomizer('../webpack.config'));
+        break;
+    default:
+        console.log('customized-config only supports "start", "build", and "test" options.');
+        process.exit(-1);
+}
+
+function loadCustomizer(module) {
+    try {
+        return require(module);
+    } catch(e) {
+        if(e.code !== "MODULE_NOT_FOUND") {
+            throw e;
+        }
+    }
+    return config => config;
+}
+
+function rewireModule(modulePath, customizer) {
+    let defaults = rewire(modulePath);
+    let config = defaults.__get__('config');
+    customizer(config);
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,31 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const ENV = process.env.STAGE || 'production';
-const IsDev = ENV === 'DEV';
+const path = require('path');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: './bundle.js'
-    },
-    resolve: {
-        modules: ['node_modules']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    query: {
-                        presets: [
-                            'react',
-                            'es2015',
-                            'stage-0'
-                        ]
-                    }
-                }
-            },
-            /*{
-                test: /(\.less|\.css$)$/,
-                loader: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['style-loader', 'css-loader', 'less-loader',  'postcss-loader']
-                })
-            },*/
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true,
-                            modules: true,
-                            localIdentName: '[local]___[hash:base64:5]'
-                        }
-                    },
-                    {
-                        loader:  'postcss-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.svg$/,
-                exclude: /node_modules/,
-                loader: 'raw-loader'
-            },
-        ]
-    },
-    /*plugins: [
-        new ExtractTextPlugin('./styles/bundle.css')
-    ]*/
-};
+module.exports = function(config) {
+    let eslintLoader = config.module.rules[0];
+    eslintLoader.use[0].options.useEslintrc = true;
+    let loaderList = config.module.rules[1].oneOf;
+    loaderList.splice(loaderList.length - 1, 0, {
+        test: /\.(less|css)$/,
+       use: [
+           require.resolve('style-loader'),
+           {
+               loader: require.resolve('css-loader'),
+               options: {
+                   importLoaders: 2
+               }
+           },
+           {
+               loader: require.resolve('postcss-loader'),
+               options: {
+                   ident: 'postcss'
+               }
+           },
+           {
+               loader: require.resolve('less-loader'),
+               options: {
+                   strictMath: true
+               }
+           }
+       ]
+    });
+}
