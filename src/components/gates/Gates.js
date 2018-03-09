3e8b556a130f231b5a7e83ca37f7f690fbcc42fe
Troshin2520 <troshin321@gmail.com>
2018-03-09 12:23:59 +0000

    added gates and way markup

---
+++ src/components/gates/Gates.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Gates.less';
+
+class Gates extends Component {
+
+    render() {
+        return (<div className="gates">
+                    <div></div>
+                </div>);
+    }
+}
+
+export default Gates;
---
+++ src/components/way/Way.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Way.less';
+
+class Way extends Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+
+        return (<div className={`way ${this.props.type}`}>
+            <div></div>
+        </div>);
+    }
+}
+
+export default Way;
